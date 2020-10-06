8ce66ab1f5c8da961e52517507c7485f17b6e5a0
Spintec <sindome123@gmail.com>
2020-10-06 14:11:10 +0200

    new adds

---
+++ Information_Security/Python/tcp_client.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+import socket
+
+clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+#used a function to get the hostname, instead of specifying it
+host = socket.gethostname()
+port= 42567
+
+clientsocket.connect(host,port)
+
+message = clientsocket.recv(1024)
+
+clientsocket.close()
