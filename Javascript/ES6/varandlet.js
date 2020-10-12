d58cd9c9cc6aeb3c9c865b2f71e3aea7533d7655
Spintec <sindome123@gmail.com>
2020-10-13 00:09:26 +0200

    Differences between var and let

---
+++ Javascript/ES6/varandlet.js
@@ -0,0 +1,12 @@
+//var camper = 'James';
+//var camper = 'David';
+//console.log(camper);
+// logs 'David'
+
+let camper = 'James';
+let camper = 'David';
+console.log(camper);
+throws and error
+"use strict";
+x=3.14;
+
