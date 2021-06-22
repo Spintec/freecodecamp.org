e4eb36a1ea67dc439935892f8eabfafd6810a306
Spintec <sindome123@gmail.com>
2021-06-22 22:26:33 +0200

    added seekndestroy.js

--- Javascript/intermediateAlgorithmScripting/seekndestroy.js
+++ Javascript/intermediateAlgorithmScripting/seekndestroy.js
@@ -7,7 +7,19 @@ Note: You have to use the arguments object.
 */
 
 function destroyer(arr) {
+    for ( let j=0; j < arr.length; j++) {   
+        for ( let i=1; i < arguments.length; i++) {
+            index_nr = arr.indexOf(arguments[i]);
+                if (index_nr!=-1) {
+                    arr.splice(index_nr, 1);
+                }
+        }
+    }
     return arr;
-  }
-  
-  console.log(destroyer([1, 2, 3, 1, 2, 3], 2, 3));
+}
+
+//console.log(destroyer([1, 2, 3, 1, 2, 3], 2, 3));
+//console.log(destroyer([1, 2, 3, 5, 1, 2, 3], 2, 3));
+//console.log(destroyer([3, 5, 1, 2, 2], 2, 3, 5));
+//console.log(destroyer([2, 3, 2, 3], 2, 3));
+console.log(destroyer(["tree", "hamburger", 53], "tree", 53));
